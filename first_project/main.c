074e639186b4d6f456adf9c51674e7f90ca2cb7f
breakbean <breakbean@163.com>
2017-05-18 17:16:11 +0800

    fist main.c

---
+++ first_project/main.c
@@ -0,0 +1,13 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
